aa468c832db39360fa0ef37bfdf177ac6b2cb322
KzeroJun <rladudwns164@gmail.com>
2024-08-03 00:53:02 +0900

    백준 2075 N번째 큰수

---
+++ src/김영준/2024_08_03/Baek2075.java
@@ -0,0 +1,42 @@
+
+import java.util.*;
+import java.io.*;
+
+public class MaBaek2075in {
+	static List<Integer> numbers;
+	static int N;
+	public static void main(String[] args) throws IOException {
+		init();
+		print();
+	}
+
+	private static void init() throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		N = Integer.parseInt(br.readLine());
+
+		numbers = new ArrayList<>();
+
+		//번호 입력
+		for(int i = 0 ; i<N; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+
+			for(int j = 0; j<N; j++) {
+				int number = Integer.parseInt(st.nextToken());
+				numbers.add(number);
+			}
+		}
+
+	}
+	private static void print() {
+
+		//내림차순 정렬
+		numbers.sort((o1,o2)->{
+			return o2 - o1;
+		});
+
+		//N번째 출력
+		System.out.println(numbers.get(N-1));
+	}
+
+}
