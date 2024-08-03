fa4b64e9dd1ea070750cb52062db923369ea502c
byeongjuPark <bottlepark28@gmail.com>
2024-08-03 13:09:06 +0900

    2075

---
+++ src/박병주/Boj2075.java
@@ -0,0 +1,35 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Collections;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+/**
+ * Boj2075
+ */
+public class Boj2075 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        int n = Integer.parseInt(br.readLine());
+        // 우선순위 큐(기본 올림차순)
+        // 역순으로 생성 - Collections.reverseOrder()
+        PriorityQueue<Integer> pQueue = new PriorityQueue<>(Collections.reverseOrder());
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < n; j++) {
+                pQueue.offer(Integer.parseInt(st.nextToken()));
+            }
+        }
+
+        for (int i = 0; i < n - 1; i++) {
+            pQueue.poll();
+        }
+
+        System.out.println(pQueue.poll());
+
+    }
+}
